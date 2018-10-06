719a7a4ed347f92c3f8246ea481170d9073a9cb2
neeta <neeta@Neeta>
2018-10-06 03:41:06 -0700

    this is the test file

---
+++ mystructure/src/test/java/mystructure/mystructure/Sample.java
@@ -0,0 +1,13 @@
+package mystructure.mystructure;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("hello this is data from framework");
+	}
+
+}
