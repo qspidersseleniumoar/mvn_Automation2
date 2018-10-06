3a424fa68d3967ea97d184971370bc7420903cf2
neeta <neeta@Neeta>
2018-10-06 03:22:03 -0700

    project is added .

---
+++ mystructure/src/main/java/mystructure/mystructure/App.java
@@ -0,0 +1,13 @@
+package mystructure.mystructure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
